8ae5c4f9ad6635ae675b14a58a532b0aeb474dea
Arjun <arjun8107@gmail.com>
2024-09-15 18:47:55 +0530

    refractor

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const { MONGODB_URI, PORT } = require("./utils/config");
 
-const Blog = require("./models/blogs");
-
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
